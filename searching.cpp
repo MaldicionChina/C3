f0a53969c78e14facaec0b04e82f3e6db4543e64
MaldicionChina <alexisrodgtz@gmail.com>
2014-02-15 09:39:36 -0500

    searchAlgoritm

---
+++ searching.cpp
@@ -0,0 +1,29 @@
+// Busqueda Secuencial
+//
+#include <array>
+
+using namespace std;
+
+class funciones
+{
+
+bool secuencial(array<int> *busqueda, int *dato, int inicio, int fin){
+	
+	for(int i=0; i<n; i++)
+	{
+		if(dato==busqueda[i]) return true;
+	}
+	return false;
+}
+
+bool binomial(array<int> *busqueda, int *dato, int inicio, int fin){
+	
+
+}
+
+}
+
+
+
+
+o
